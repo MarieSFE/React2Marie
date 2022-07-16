8cc50aeebdd142fefcae44bcdb584c87cd598c8a
Marie Preti <marielapreti@gmail.com>
2022-07-16 08:20:54 -0300

    new branch

--- src/App.js
+++ src/App.js
@@ -4,19 +4,12 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <header className="App-header OtraClase">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Bienvenidos
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button style={{color: 'pink', backgroundColor: 'blue', fontSize: '30px'}}>Boton</button>
       </header>
     </div>
   );
