bfca53a030dda7deb239ccbc73d5342e88bcac64
Marie Preti <marielapreti@gmail.com>
2022-08-28 12:54:56 +0200

    en proceso

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -3,6 +3,8 @@ import { useState, useEffect } from 'react'
 import { getProducts, getProductsByCategory } from "../../asyncMock"
 import ItemList from '../ItemList/ItemList'
 import { useParams } from 'react-router-dom' 
+import {getDocs, collection} from 'firebase/firestore'
+import {db} from '../../service/firebase'
 
 const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
@@ -11,24 +13,41 @@ const ItemListContainer = ({ greeting }) => {
     const { categoryId } = useParams()   
     useEffect(() => {
         setLoading(true)
-        const asyncFunction = categoryId ? getProductsByCategory : getProducts
+
+        getDocs (collection(db, 'products')).then(response => {
+            console.log(response)
+            const productsAdapted = response.docs.map(doc => {
+                const data = doc.data() 
+                console.log(data)
+                return { id: doc.id, ...data}
+            })
+            console.log(productsAdapted)
+        }). catch (error=> {
+            console.log (error)
+        }).finally (()=>
+            setLoading(false))
+        })
+
+}, [categoryId]),
+
+        //const asyncFunction = categoryId ? getProductsByCategory : getProducts
         
-        asyncFunction(categoryId).then(response => {
-            setProducts(response)
-        }).catch(error => {
-            console.log(error)
-        }).finally(() => {
-            setLoading(false)
-        })  
-    }, [categoryId])
+        //asyncFunction(categoryId).then(response => {
+        //    setProducts(response)
+        //}).catch(error => {
+        //    console.log(error)
+        //}).finally(() => {
+        //    setLoading(false)
+        //})  
+    
 
     return (
         <div onClick={() => console.log('click en itemlistcontainer')}>
             <h1>{`${greeting} ${categoryId || ''}`}</h1>
-            {/* <button onClick={(e) => console.log(e)}>boton</button> */}
+          
             <ItemList products={products} />
         </div>
     )
-}
+
 
 export default ItemListContainer
