4b4354f445eeb28106cab7cfed1b35cd61f155a2
Marie Preti <marielapreti@gmail.com>
2022-08-28 13:16:37 +0200

    intentando cartview

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import ItemListContainer from './components/ItemListContainer/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { CartContextProvider } from './context/CartContext'
+import { NotificationProvider } from './notification/Notification'
 
 
 
@@ -13,19 +14,21 @@ function App() {
 
   return (
     <div className="App"> 
-    <CartContextProvider>
-      <BrowserRouter>
-        <Navbar />
+    <NotificationProvider>
+      <CartContextProvider>
+        <BrowserRouter>
+            <Navbar />
 
-        <img alt="logo" src={logoImg} />
-        <Routes>
-          <Route path='/' element={<ItemListContainer greeting='Listado de todos los productos'/>}/>
-          <Route path='/category/:categoryId' element={<ItemListContainer greeting='Listado filtrado'/>} />
-          <Route path='/detail/:productId' element={<ItemDetailContainer />} />  
-          <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
-        </Routes>
-      </BrowserRouter>
+            <img alt="logo" src={logoImg} />
+            <Routes>
+              <Route path='/' element={<ItemListContainer greeting='Listado de todos los productos'/>}/>
+              <Route path='/category/:categoryId' element={<ItemListContainer greeting='Listado filtrado'/>} />
+              <Route path='/detail/:productId' element={<ItemDetailContainer />} />  
+              <Route path='*' element={<h1>404 NOT FOUND</h1>} /> 
+            </Routes>
+        </BrowserRouter>
       </CartContextProvider>
+    </NotificationProvider>
     </div>
   );
 }
--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -8,7 +8,24 @@ const ItemListContainer = ({ greeting }) => {
     const [products, setProducts] = useState([])
     const [loading, setLoading] = useState(true)
 
-    const { categoryId } = useParams()   
+    const [title, setTitle] = useState('primer titulo')
+
+    const { categoryId } = useParams()
+
+    // useEffect(() => {
+    //     const onResize = (e) => {
+    //         console.log(e)
+    //         console.log('cambio tamaño de ventana')
+    //     }
+    //     window.addEventListener('resize', onResize)
+
+        
+    //     return () => {
+    //         window.removeEventListener('resize', onResize)
+    //     }
+    // }, [])
+
+
     useEffect(() => {
         setLoading(true)
         const asyncFunction = categoryId ? getProductsByCategory : getProducts
@@ -22,9 +39,24 @@ const ItemListContainer = ({ greeting }) => {
         })  
     }, [categoryId])
 
+    useEffect(() => {
+        setTimeout(() => {
+            setTitle('segundo titulo')
+        }, 3000)
+    })
+
+    if(loading) {
+        return <h1>Cargando productos...</h1>
+    }
+
+    // if(products.length === 0) {
+    //     return categoryId ? <h1>No hay productos en nuestra categoria {categoryId}</h1> : <h1>No hay productos disponibles</h1>
+    // }
+
     return (
         <div onClick={() => console.log('click en itemlistcontainer')}>
             <h1>{`${greeting} ${categoryId || ''}`}</h1>
+            <h2>{title}</h2>
             {/* <button onClick={(e) => console.log(e)}>boton</button> */}
             <ItemList products={products} />
         </div>
